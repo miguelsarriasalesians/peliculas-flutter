56856926359aeda1a173a125bedfeb083793f6f4
Miguel Peña <miguel.pena@sarria.salesians.cat>
2020-11-07 11:20:21 +0100

    Adaptada funcion buscarPelicula para buscar series

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -116,7 +116,7 @@ class PeliculasProvider {
 
   Future<List<Pelicula>> buscarPelicula( String query ) async {
 
-    final url = Uri.https(_url, '3/search/movie', {
+    final url = Uri.https(_url, '3/search/tv', {
       'api_key'  : _apikey,
       'language' : _language,
       'query'    : query
